cb9d153712c4208a6ebca016702f297e254ed486
ruriknj <ruriknj@hotmail.com>
2020-05-25 23:04:58 -0300

    Ajustes no endpoint /categorias/{id} e exceptions

--- src/main/java/com/rurik/cursomc/services/CategoriaService.java
+++ src/main/java/com/rurik/cursomc/services/CategoriaService.java
@@ -1,24 +1,25 @@
 package com.rurik.cursomc.services;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rurik.cursomc.domain.Categoria;
 import com.rurik.cursomc.repositories.CategoriaRepository;
+import com.rurik.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id){
-		
+	public Categoria find(Integer id) {
+
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElse(null);
-		
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 }
