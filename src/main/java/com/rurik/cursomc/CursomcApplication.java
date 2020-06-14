eb8c0877186e42806daadd16397d637e65e266f9
Rurik Janiszewski <ruriknj@hotmail.com>
2020-06-14 12:04:20 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/rurik/cursomc/CursomcApplication.java
+++ src/main/java/com/rurik/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.rurik.cursomc;
 
+import java.text.SimpleDateFormat;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +14,20 @@ import com.rurik.cursomc.domain.Cidade;
 import com.rurik.cursomc.domain.Cliente;
 import com.rurik.cursomc.domain.Endereco;
 import com.rurik.cursomc.domain.Estado;
+import com.rurik.cursomc.domain.Pagamento;
+import com.rurik.cursomc.domain.PagamentoComBoleto;
+import com.rurik.cursomc.domain.PagamentoComCartao;
+import com.rurik.cursomc.domain.Pedido;
 import com.rurik.cursomc.domain.Produto;
+import com.rurik.cursomc.domain.enums.EstadoPagamento;
 import com.rurik.cursomc.domain.enums.TipoCliente;
 import com.rurik.cursomc.repositories.CategoriaRepository;
 import com.rurik.cursomc.repositories.CidadeRepository;
 import com.rurik.cursomc.repositories.ClienteRepository;
 import com.rurik.cursomc.repositories.EnderecoRepository;
 import com.rurik.cursomc.repositories.EstadoRepository;
+import com.rurik.cursomc.repositories.PagamentoRepository;
+import com.rurik.cursomc.repositories.PedidoRepository;
 import com.rurik.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +45,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -83,6 +96,24 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), e1, cli1);
+		
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), e2, cli1);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 }
---
+++ src/main/java/com/rurik/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.rurik.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.rurik.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
